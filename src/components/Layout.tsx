b03065b461b4cc488503393dbcfcde7329fca978
GilbertoSVN <nvotreblig@gmail.com>
2022-07-18 15:43:26 -0300

    chore: add bee image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
 import { ReactNode } from 'react'
+import Bee from "../assets/bee.svg"
 
 interface LayoutProps {
   children: ReactNode
+  hasLogo?: boolean
   isCentered?: boolean
 }
 
-function Layout({ children, isCentered = false }: LayoutProps) {
+function Layout({ children, hasLogo = false, isCentered = false }: LayoutProps) {
   return (
-    <Box display='flex' w='100vw' h='100vh' backgroundColor="#F2EC54" justifyContent={isCentered ? 'center' : 'normal'}>
-      {children}
-    </Box>
+      <Box display='flex' w='100vw' h='100vh' backgroundColor="#F2EC54" justifyContent={isCentered ? 'center' : 'normal'}>
+        {children}
+        {hasLogo && <Image src={Bee} position="absolute" left="0" bottom="0" boxSize={["100px", "200px", "300px"]} />}
+      </Box>
   )
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,7 @@ function Login() {
   }
 
   return (
-    <Layout isCentered={true}>
+    <Layout isCentered={true} hasLogo={true}>
       <Box
         display='flex'
         justifyContent='center'
